aa5f11bf7ce80ca28a883d1152147702c633ce3f
hugoolov <hugo@harnaes.no>
2025-11-06 15:03:12 +0100

    jobber med gateway og consul

--- auth/src/main/java/com/example/auth/UserRepo.java
+++ auth/src/main/java/com/example/auth/UserRepo.java
@@ -1,8 +1,10 @@
 package com.example.auth;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+@Repository
 public interface UserRepo extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
